d970086d60ad447a3dd1f0df39532faccd3e8d9a
Akil <akileswarkrishnasamy@gmail.com>
2025-06-10 22:09:41 +0530

    changed db configuration

--- src/config/database.py
+++ src/config/database.py
@@ -4,14 +4,11 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-ROOT = os.getenv('DATABASE_USERNAME')
-PASSWORD = os.getenv('DATABASE_PASSWORD')
-DATABASE = os.getenv('DATABASE_NAME')
-PORT = os.getenv('DATABASE_PORT')
 
-SQL_DB_URL = f"mysql+mysqldb://{ROOT}:{PASSWORD}@localhost:{PORT}/{DATABASE}"
 
-engine = create_engine(SQL_DB_URL)
+SQL_DB_URL = os.getenv('DB_URL')
+
+engine = create_engine(SQL_DB_URL) # type: ignore
 session_local = sessionmaker(autocommit = False, autoflush= False, bind=engine)
 
 Base = declarative_base()
